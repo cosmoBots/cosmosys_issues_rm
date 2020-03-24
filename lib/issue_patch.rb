4a54d14ec6d09490560e5222e9f457a66c8a6e01
Txinto <txinto@gatatac.org>
2020-03-24 12:48:02 +0000

    Automatic chapter generation

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -11,8 +11,9 @@ module IssuePatch
     # Same as typing in the class 
     base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
-      before_save :check_identifier
+      #before_save :check_identifier
       before_validation :check_identifier
+      after_save :check_chapter
     end
 
   end
@@ -21,32 +22,62 @@ module IssuePatch
   end
   
   module InstanceMethods
-    @@cfprisscount = ProjectCustomField.find_by_name('IssCounter')    
-    @@cfprissprefix = ProjectCustomField.find_by_name('IssPrefix')    
+    @@cfprjcount = ProjectCustomField.find_by_name('IssCounter')    
+    @@cfprjprefix = ProjectCustomField.find_by_name('IssPrefix')  
+    @@cfisschapter = IssueCustomField.find_by_name('IssChapter')  
 
-    # Wraps the association to get the Deliverable subject.  Needed for the 
-    # Query and filtering
     def check_identifier
+      # AUTO SUBJECT
       if self.subject == "" or self.subject == nil then
-        if @@cfprisscount != nil then
-          cfprisscount = self.project.custom_values.find_by_custom_field_id(@@cfprisscount.id)
-          if cfprisscount != nil then
-            if @@cfprissprefix != nil then
-              cfprissprefix = self.project.custom_values.find_by_custom_field_id(@@cfprissprefix.id)
-              if cfprissprefix != nil then
-                self.subject = cfprissprefix.value+"-"+format('%04d', cfprisscount.value)
-                cfprisscount.value = (cfprisscount.value.to_i+1)
-                cfprisscount.save
+        if @@cfprjcount != nil then
+          cfprjcount = self.project.custom_values.find_by_custom_field_id(@@cfprjcount.id)
+          if cfprjcount != nil then
+            if @@cfprjprefix != nil then
+              cfprjprefix = self.project.custom_values.find_by_custom_field_id(@@cfprjprefix.id)
+              if cfprjprefix != nil then
+                self.subject = cfprjprefix.value+"-"+format('%04d', cfprjcount.value)
+                cfprjcount.value = (cfprjcount.value.to_i+1)
+                cfprjcount.save
               end
             end
           end
+        end
+      end
+
+      return true 
+    end
+
+    def check_chapter
+      # AUTO CHAPTER
+      if @@cfisschapter != nil then
+        cfisschapter = self.custom_values.find_by_custom_field_id(@@cfisschapter.id)
+        if cfisschapter == nil then
+          cfisschapter = CustomValue.new
+          cfisschapter.custom_field = @@cfisschapter
+          cfisschapter.customized = self
+          cfisschapter.value = nil
+          print(hola0)
+        end
+        if cfisschapter.value == "" or cfisschapter.value == nil then
+          if self.parent != nil then
+            cfparentiffchapter = self.parent.custom_values.find_by_custom_field_id(@@cfisschapter.id)
+            if cfparentiffchapter == nil then
+              self.parent.save
+            end
+            cfisschapter.value = cfparentiffchapter.value+"z."
+          else
+            if @@cfprjprefix != nil then
+              cfprjprefix = self.project.custom_values.find_by_custom_field_id(@@cfprjprefix.id)
+            end
+            cfisschapter.value = cfprjprefix.value+"-z.1."
+          end
+          cfisschapter.save
         end
       end
       return true 
     end
   end    
 end
-
 # Add module to Issue
 Issue.send(:include, IssuePatch)
 
