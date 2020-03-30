0fdbfe23861784b20b999e05b0c734dfd3ea6f33
cosmosys reqbot <cosmobots.eu@gmail.com>
2020-03-30 10:13:10 +0000

    First implementation is working, TODO: still hardcoded to target_ids 5 and 6

--- assets/pythons/IssReports.py
+++ assets/pythons/IssReports.py
@@ -312,6 +312,11 @@ for tk in data['targets']:
     t = data['targets'][tk]
     t['issues'] = []
     if 'start_date' in t.keys():
+        #print(t)
+        #print(t['start_date'])
+        #print(datetime.strptime(t['start_date'], '%Y-%m-%d'))
+        #print(datetime.strptime(t['start_date'], '%Y-%m-%d').timestamp())
+        #print(int(datetime.strptime(t['start_date'], '%Y-%m-%d').timestamp()))
         t['start_date_int'] = int(datetime.strptime(t['start_date'], '%Y-%m-%d').timestamp())
     if 'due_date' in t.keys():
         t['due_date_int'] = int(datetime.strptime(t['due_date'], '%Y-%m-%d').timestamp())
@@ -319,9 +324,16 @@ for tk in data['targets']:
 
 for r in issueslist:
     if 'start_date' in r.keys():
-        r['start_date_int'] = int(datetime.strptime(r['start_date'], '%Y-%m-%d').timestamp())
+        if(r['start_date']):
+            print(r)
+            print(r['start_date'])
+            print(datetime.strptime(r['start_date'], '%Y-%m-%d'))
+            print(datetime.strptime(r['start_date'], '%Y-%m-%d').timestamp())
+            print(int(datetime.strptime(r['start_date'], '%Y-%m-%d').timestamp()))
+            r['start_date_int'] = int(datetime.strptime(r['start_date'], '%Y-%m-%d').timestamp())
     if 'due_date' in r.keys():
-        r['due_date_int'] = int(datetime.strptime(r['due_date'], '%Y-%m-%d').timestamp())
+        if(r['due_date']):
+            r['due_date_int'] = int(datetime.strptime(r['due_date'], '%Y-%m-%d').timestamp())
 
     if 'type' in r.keys():
         if r['type'] != 'Info':
@@ -383,8 +395,19 @@ for r in issueslist:
                             data['byperson'][personkey]['targets'][p] = {}
                             data['byperson'][personkey]['targets'][p]['assigned'] = []
                             data['byperson'][personkey]['targets'][p]['supervised'] = []
-                        
-                    for p in periods:                            
+                    print("*********************1")
+                    print(periods)
+                    print("*********************2")
+                    print(data['byperson'][personkey]['targets'].keys())                        
+                    print("*********************3")
+                    print(r)
+                    print("*********************4")
+                    for p in periods:
+                        if (p not in data['byperson'][personkey]['targets'].keys()):
+                            data['byperson'][personkey]['targets'][p] = {}
+                            data['byperson'][personkey]['targets'][p]['assigned'] = []
+                            data['byperson'][personkey]['targets'][p]['supervised'] = []
+
                         data['byperson'][personkey]['targets'][p]['assigned'].append(r)
 
             if 'supervisor' in r.keys():
@@ -399,7 +422,16 @@ for r in issueslist:
                         data['byperson'][personkey]['targets'][p]['assigned'] = []
                         data['byperson'][personkey]['targets'][p]['supervised'] = []
                         
+                print("*********************")
+                print(periods)
+                print("*********************")
+                print(data['byperson'][personkey]['targets'].keys())
                 for p in periods:                            
+                    if (p not in data['byperson'][personkey]['targets'].keys()):
+                        data['byperson'][personkey]['targets'][p] = {}
+                        data['byperson'][personkey]['targets'][p]['assigned'] = []
+                        data['byperson'][personkey]['targets'][p]['supervised'] = []
+
                     data['byperson'][personkey]['targets'][p]['supervised'].append(r)
 
 
@@ -560,19 +592,22 @@ with open(reporting_path + '/doc/issues.json', 'w') as outfile:
 
 from Naked.toolshed.shell import execute_js
 
-datelim11 = int(datetime.strptime(data['targets']['1']['start_date'], '%Y-%m-%d').timestamp())
-datelim12 = int(datetime.strptime(data['targets']['1']['due_date'], '%Y-%m-%d').timestamp())
-datelim21 = int(datetime.strptime(data['targets']['2']['start_date'], '%Y-%m-%d').timestamp())
-datelim22 = int(datetime.strptime(data['targets']['2']['due_date'], '%Y-%m-%d').timestamp())
-print("Datelim11: ",data['targets']['1']['start_date']," ",datelim11)
-print("Datelim12: ",data['targets']['1']['due_date']," ",datelim12)
-print("Datelim21: ",data['targets']['2']['start_date']," ",datelim21)
-print("Datelim22: ",data['targets']['2']['due_date']," ",datelim22)
+period='5'
+nextPeriod='6'
+
+datelim11 = int(datetime.strptime(data['targets'][period]['start_date'], '%Y-%m-%d').timestamp())
+datelim12 = int(datetime.strptime(data['targets'][period]['due_date'], '%Y-%m-%d').timestamp())
+datelim21 = int(datetime.strptime(data['targets'][nextPeriod]['start_date'], '%Y-%m-%d').timestamp())
+datelim22 = int(datetime.strptime(data['targets'][nextPeriod]['due_date'], '%Y-%m-%d').timestamp())
+print("Datelim11: ",data['targets'][period]['start_date']," ",datelim11)
+print("Datelim12: ",data['targets'][period]['due_date']," ",datelim12)
+print("Datelim21: ",data['targets'][nextPeriod]['start_date']," ",datelim21)
+print("Datelim22: ",data['targets'][nextPeriod]['due_date']," ",datelim22)
 
 
 success = execute_js('./plugins/cosmosys_issues/assets/pythons/lib/launch_carbone.js', reporting_path
     + " " + data['project']['identifier'] + " \'" + data['project']['name'] + "\' "+"0"
-    + " " + '1' + " " + '2'
+    + " " + period + " " + nextPeriod
     + " " + str(datelim11) + " " + str(datelim12)
     + " " + str(datelim21) + " " + str(datelim22)
     )
@@ -593,7 +628,7 @@ for person in data['byperson'].keys():
     print(person)
     success = execute_js('./plugins/cosmosys_issues/assets/pythons/lib/launch_carbone.js', reporting_path
         + " " + person + " " + person + " "+"1"
-        + " " + '1' + " " + '2'
+        + " " + period + " " + nextPeriod
         + " " + str(datelim11) + " " + str(datelim12)
         + " " + str(datelim21) + " " + str(datelim22)
         )
